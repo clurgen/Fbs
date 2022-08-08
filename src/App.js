d42e597b9b3ef45ebe7ed50879462a35d28fe3dd
clement <clementdu954@live.fr>
2022-08-08 09:04:36 +0200

    commit du 07/08 en retard ^^'

--- src/App.js
+++ src/App.js
@@ -1,21 +1,27 @@
-import React from 'react';
-import './App.css';
-import {Routes, Route} from "react-router-dom";
-import Creation from "./components/Article/CreationArticle";
-import Article from "./components/Article/Article";
-import Articles from "./components/Article/LesArticles";
-import Navbar from './components/Navbar/Navbar';
-
+import React from "react";
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Creation from "./components/views/Article/CreationArticle";
+import Update from "./components/views/Article/UpdateArticle";
+import Article from "./components/views/Article/Article";
+import Articles from "./components/views/Article/LesArticles";
+import Navbar from "./components/views/Navbar/Navbar";
+import Connexion from "./components/views/User/Connexion";
+import Inscription from "./components/views/User/Inscription";
 
 function App() {
   return (
     <div className="App">
-    <Navbar></Navbar>
-    <Routes>
-      <Route path="/" element={<Creation />} />
-      <Route path="/article/:id/" element={<Article/>} />
-      <Route path="/articles" element={<Articles/>} />
-    </Routes>
+      <Navbar></Navbar>
+      <Routes>
+        <Route path="/article/add" element={<Creation />} />
+        <Route path="/article/:id/" element={<Article />} />
+        <Route path="/article/edit/:id/" element={<Update />} />
+        <Route path="/articles" element={<Articles />} />
+
+        <Route path="/connexion" element={<Connexion />} />
+        <Route path="/inscription" element={<Inscription />} />
+      </Routes>
     </div>
   );
 }
